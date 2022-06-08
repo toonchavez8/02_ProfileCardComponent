659c335876d730d49f6529c7801a9621c4e0562f
Miguel Chavez <toonchavez8@gmail.com>
2022-06-08 14:15:48 -0500

    Emptied JS removed Hover as it bugged out

--- JS/Animation.js
+++
@@ -1,9 +0,0 @@
-$( ".bodyCard" ).toggle({ effect: "scale", persent: 25 });
-
-
-
-$('.bodyCard').mouseover(function(){
-    $(this).stop().animate({width: (current_w * 3), height: (current_h * 3)}, 200,'easeInQuint');
-  }).mouseout(function(){ 
-    $(this).stop().animate({"width": "30%","left":"0px"}, 200,'easeInQuint');
-  });
